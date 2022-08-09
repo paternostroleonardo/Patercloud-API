8c1e3f5fbcc659adf728f5f92cce8131e94614f3
paternostroleonardo <paternostroleonardo@gmail.com>
2022-08-09 14:14:56 -0500

    react auth

---
+++ frontend-react/react-redux-hooks-jwt-auth/src/components/Profile.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Redirect } from 'react-router-dom';
+import { useSelector } from "react-redux";
+
+const Profile = () => {
+  const { user: currentUser } = useSelector((state) => state.auth);
+
+  if (!currentUser) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>
+          <strong>{currentUser.name}</strong> Profile
+        </h3>
+      </header>
+      <p>
+        <strong>Token:</strong> {currentUser.accessToken} ...{" "}
+        {currentUser.accessToken}
+      </p>
+      <p>
+        <strong>Id:</strong> {currentUser.id}
+      </p>
+      <p>
+        <strong>Email:</strong> {currentUser.email}
+      </p>
+      <strong>Authorities:</strong>
+    </div>
+  );
+};
+
+export default Profile;
